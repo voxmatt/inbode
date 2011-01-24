61f2824ef5335a681a00018ccd6d0ea92ab9b079
Ivan Stegic <ivan.stegic@gmail.com>
2011-01-23 22:42:20 -0600

    Actually added db info to the dbdeploy file this time.

---
+++ dbdeploy.php
@@ -0,0 +1,14 @@
+<?php
+
+	$dbdeploysettings = array( 
+														'inbode.ten7i.net' => array(
+																						'deploy' => 1,
+																						'sql' => 'inbode.com/sql/drupaldb.sql',
+																						'username' => 'stegic_22',
+																						'password' => 'kp9DncwZ',
+																						'host' => 'db137c.pair.com',
+																						'database' => 'stegic_inbode'
+																						),
+														)
+																												
+?>
