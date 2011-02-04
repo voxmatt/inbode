756fc7b6e3d371dc3ed5eff73325fa865f1c325f
Ivan Stegic <ivan.stegic@gmail.com>
2011-02-04 15:39:56 -0600

    added matt and charlie as actual users; tweaked front page so it's not password protected; added a password for everything else except the login screen.

--- inbode.com/src/sites/all/themes/inbode/page-front.tpl.php
+++ inbode.com/src/sites/all/themes/inbode/page-front.tpl.php
@@ -1,6 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html style="background-color:#1693A5;width:100%;height:100%;" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 <!-- 
 
          _       __              __   
@@ -19,148 +20,29 @@
 
 
  -->
-  <title>Inbode</title>
-  <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-  <meta http-equiv="content-type" content="text/html; charset=us-ascii">
-  <link type="text/css" href="/ui/css/custom-theme/jquery-ui-1.8.2.custom.css" rel="stylesheet">
-  <link type="text/css" href="/ui/css/screen.css" rel="stylesheet">
-  <link type="text/css" href="/ui/css/jquery.fancybox-1.3.4.css" rel="stylesheet">  
-  <script type="text/javascript" src="/ui/js/jquery-1.4.2.min.js"></script>
-  <script type="text/javascript" src="/ui/js/jquery-ui-1.8.2.custom.min.js"></script>
-  <script type="text/javascript" src="/ui/js/jquery.cookie.js"></script>
-  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
-  <script type="text/javascript" src="/ui/js/jquery.checkbox.js"></script>
-  <script type="text/javascript" src="http://code.google.com/apis/gears/gears_init.js"></script>
-  <script type="text/javascript" src="http://www.google.com/jsapi"></script>
-  <script type="text/javascript" src="/ui/js/jquery.fancybox-1.3.4.pack.js"></script>
-  <script type="text/javascript" src="/ui/js/jquery.easing-1.3.pack.js"></script>
-  <script type="text/javascript" src="/ui/js/inbode.js"></script>
-  <script type="text/javascript">
-		google.load('gdata', '2.x', { packages: ['maps'] });
-  </script>
-</head>
+ 
+ 	<head>
 
-<body>
-  <div id="map_canvas"></div>
+		<meta name="author" content="Inbode Inc." />
+		<title>inbode - easy online apartment search</title>
+		<?php print $head; ?>
+		<?php print $styles; ?>
+		<?php print $scripts; ?>
 
-  <div id="t7_container">
-    <div id="t7_ui_container">
-      <div class="top_cap"></div><!-- TOP_CAP END -->
+	</head>
+	<body style="background-color:#1693A5;width:100%;height:100%;background-image: url(/<?php echo path_to_theme(); ?>/images/header-front-bg.png);background-repeat: repeat-x;">
+  		 <div style="width:800px;margin: 0 auto; padding:0;height:100%;background-color:transparent;text-align:center;">
+<img style="width:230px;margin:42px auto 0 auto;" src="/<?php echo path_to_theme(); ?>/images/inbode-logo.png" border="0" width="230" height="79" alt="inbode" title="inbode" /><h2 style="text-align:center;color:#fff;text-transform:lowercase;font-weight:lighter;">easy online apartment search</h2>
 
-      <div class="body_content">
-        <!-- ============ Logo & Search Start ============ -->
+<form action="http://tenseveninteractive.createsend.com/t/y/s/ukdujy/" method="post" id="subForm">
+<div><p style="color:#fff;text-align:center;margin-top:50px;">sign up for our public beta, and we'll<br>give you a sneak peek when a slot opens up</p>
+<input value="email address" type="text" name="cm-ukdujy-ukdujy" id="ukdujy-ukdujy" /><br />
+<input type="submit" value="subscribe" style="margin-top:5px;" />
+</div>
+</form>
 
-        <div class="logo_search">
-          <div id="t7_signin">
-            <a href="/user">Sign in</a>
-          </div><!-- t7_signin end -->
+</div> 
 
-          <div class="t7_city">
-            <input id="t7_city" maxlength="50" value="City or Zip">
-          </div><!-- t7_city end -->
-
-          <div id="t7_filter_selector">
-            <div id="t7_ldr"><img border="0" width="16" height="16" src=
-            "/ui/img/ajax-loader.gif"></div><!-- t7_ldr end -->
-            <a id="filtz" class="trigger_1 inactive" href="#">filters</a>
-          </div><!-- t7_filter_selector end -->
-        </div><!-- LOGO_SEARCH END -->
-        <!-- ============ First Filter Set Start ============ -->
-
-        <div class="first_set">
-          <div id="main_slider_container">
-            <div class="t7_vert_slider_container price">
-              <div class="t7_slider_title">
-                price
-              </div><!-- t7_slider_title end -->
-
-              <div class="t7_slider_holder">
-                <div id="slider-price" class="t7_slider"></div>
-                <!-- t7_slider & slider-price end -->
-              </div><!-- t7_slider_holder end -->
-            </div><!-- t7_vert_slider_container end -->
-
-            <div class="t7_vert_slider_container beds">
-              <div class="t7_slider_title">
-                beds
-              </div>
-
-              <div class="t7_slider_holder">
-                <div id="slider-beds" class="t7_slider"></div>
-              </div><!-- t7_slider_holder end -->
-            </div><!-- t7_vert_slider_container end -->
-
-            <div class="t7_vert_slider_container baths">
-              <div class="t7_slider_title">
-                baths
-              </div>
-
-              <div class="t7_slider_holder">
-                <div id="slider-baths" class="t7_slider"></div>
-              </div><!-- t7_slider_holder end -->
-            </div><!-- t7_vert_slider_container end -->
-          </div><!-- main_slider_container end -->
-
-          <div id="t7_more_filters">
-            <a class="trigger_2 inactive" href="#">more filters</a>
-          </div><!-- t7_more_filters end -->
-        </div><!-- 1ST_SET END -->
-        <!-- ============ Second Filter Set Start ============ -->
-
-        <div class="second_set">
-          <div id="t7_dates">
-            move-in from <span id="date-from"></span> to <span id="date-to"></span>
-
-            <div id="slider-dates"></div><!-- slider-dates end -->
-          </div><!-- t7_dates end -->
-
-          <div class="amenities" id="amenities">
-            <ul class="left_col">
-              <li><input class="jquery-safari-checkbox" type="checkbox" name="amenities"
-              id="unit_am_balcony"><label for="unit_am_balcony">balcony</label></li>
-
-              <li><input class="jquery-safari-checkbox" type="checkbox" name="amenities"
-              id="unit_am_dishwasher"><label for=
-              "unit_am_dishwasher">dishwasher</label></li>
-
-              <li><input class="jquery-safari-checkbox" type=
-              "checkbox" name="amenities" id="unit_am_furnished"><label for=
-              "unit_am_furnished">furnished</label></li>
-
-              <li><input class="jquery-safari-checkbox" type=
-              "checkbox" name="amenities" id="unit_am_laundry"><label for=
-              "unit_am_laundry">unit laundry</label></li>
-
-              <li><input class="jquery-safari-checkbox" type=
-              "checkbox" name="amenities" id="unit_am_disposal"><label for=
-              "unit_am_disposal">disposal</label></li>
-            </ul>
-
-            <ul class="right_col">
-              <li><input class="jquery-safari-checkbox" type=
-              "checkbox" name="amenities" id="unit_am_garage"><label for=
-              "unit_am_garage">garage</label></li>
-
-              <li><input class="jquery-safari-checkbox" type=
-              "checkbox" name="amenities" id="building_am_pool"><label for=
-              "building_am_pool">pool</label></li>
-
-              <li><input class="jquery-safari-checkbox" type=
-              "checkbox" name="amenities" id="building_am_cats"><label for=
-              "building_am_cats">cats</label></li>
-
-              <li><input class="jquery-safari-checkbox" type=
-              "checkbox" name="amenities" id="building_am_dogs_small"><label for=
-              "building_am_dogs_small">small dogs</label></li>
-
-              <li><input class="jquery-safari-checkbox" type=
-              "checkbox" name="amenities" id="building_am_dogs_large"><label for=
-              "building_am_dogs_large">large dogs</label></li>
-            </ul>
-          </div><!-- amenities end -->
-        </div><!-- 2ND_SET END -->
-      </div><!-- BODY_CONTENT END -->
-    </div><!-- t7_ui_CONTAINER END -->
-  </div>
-</body>
+		<?php print $closure; ?>
+	</body>
 </html>
---
+++ inbode.com/src/sites/all/themes/inbode/page-node-144.tpl.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- 
+
+         _       __              __   
+        (_)___  / /_  ____  ____/ /__ 
+       / / __ \/ __ \/ __ \/ __  / _ \
+      / / / / / /_/ / /_/ / /_/ /  __/
+     /_/_/ /_/_.___/\____/\__,_/\___/ 
+
+     inbode.com is made in America
+     by the people of ten7i.com
+       ______          ______                      
+      /_  __/___  ____/__  (_) _________  ____ ___ 
+       / /  / _ \/ __ \ / / / / ___/ __ \/ __ `__ \
+      / /  /  __/ / / // / /_/ /__/ /_/ / / / / / /
+     /_/   \___/_/ /_//_/_/(_)___/\____/_/ /_/ /_/ 
+
+
+ -->
+  <title>inbode - easy online apartment search</title>
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+  <meta http-equiv="content-type" content="text/html; charset=us-ascii">
+  <link type="text/css" href="/ui/css/custom-theme/jquery-ui-1.8.2.custom.css" rel="stylesheet">
+  <link type="text/css" href="/ui/css/screen.css" rel="stylesheet">
+  <link type="text/css" href="/ui/css/jquery.fancybox-1.3.4.css" rel="stylesheet">  
+  <script type="text/javascript" src="/ui/js/jquery-1.4.2.min.js"></script>
+  <script type="text/javascript" src="/ui/js/jquery-ui-1.8.2.custom.min.js"></script>
+  <script type="text/javascript" src="/ui/js/jquery.cookie.js"></script>
+  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+  <script type="text/javascript" src="/ui/js/jquery.checkbox.js"></script>
+  <script type="text/javascript" src="http://code.google.com/apis/gears/gears_init.js"></script>
+  <script type="text/javascript" src="http://www.google.com/jsapi"></script>
+  <script type="text/javascript" src="/ui/js/jquery.fancybox-1.3.4.pack.js"></script>
+  <script type="text/javascript" src="/ui/js/jquery.easing-1.3.pack.js"></script>
+  <script type="text/javascript" src="/ui/js/inbode.js"></script>
+  <script type="text/javascript">
+		google.load('gdata', '2.x', { packages: ['maps'] });
+  </script>
+</head>
+
+<body>
+  <div id="map_canvas"></div>
+
+  <div id="t7_container">
+    <div id="t7_ui_container">
+      <div class="top_cap"></div><!-- TOP_CAP END -->
+
+      <div class="body_content">
+        <!-- ============ Logo & Search Start ============ -->
+
+        <div class="logo_search">
+        	<div id="t7_mapleft">
+        		<a href="http://blog.inbode.com/">Blog</a>
+        	</div>
+          <div id="t7_signin">
+						<?php 
+            
+            global $user;
+            
+            
+            if ($user->roles[2]) {
+            	print '<a href="/user/'.$user->uid.'/inbode">My account</a>';
+            } else {
+            	print '<a href="/user/login">Sign in</a>';
+            }
+            
+            
+            
+            ?>
+          </div><!-- t7_signin end -->
+
+          <div class="t7_city">
+            <input id="t7_city" maxlength="50" value="City or Zip">
+          </div><!-- t7_city end -->
+
+          <div id="t7_filter_selector">
+            <div id="t7_ldr"><img border="0" width="16" height="16" src=
+            "/ui/img/ajax-loader.gif"></div><!-- t7_ldr end -->
+            <a id="filtz" class="trigger_1 inactive" href="#">filters</a>
+          </div><!-- t7_filter_selector end -->
+        </div><!-- LOGO_SEARCH END -->
+        <!-- ============ First Filter Set Start ============ -->
+
+        <div class="first_set">
+          <div id="main_slider_container">
+            <div class="t7_vert_slider_container price">
+              <div class="t7_slider_title">
+                price
+              </div><!-- t7_slider_title end -->
+
+              <div class="t7_slider_holder">
+                <div id="slider-price" class="t7_slider"></div>
+                <!-- t7_slider & slider-price end -->
+              </div><!-- t7_slider_holder end -->
+            </div><!-- t7_vert_slider_container end -->
+
+            <div class="t7_vert_slider_container beds">
+              <div class="t7_slider_title">
+                beds
+              </div>
+
+              <div class="t7_slider_holder">
+                <div id="slider-beds" class="t7_slider"></div>
+              </div><!-- t7_slider_holder end -->
+            </div><!-- t7_vert_slider_container end -->
+
+            <div class="t7_vert_slider_container baths">
+              <div class="t7_slider_title">
+                baths
+              </div>
+
+              <div class="t7_slider_holder">
+                <div id="slider-baths" class="t7_slider"></div>
+              </div><!-- t7_slider_holder end -->
+            </div><!-- t7_vert_slider_container end -->
+          </div><!-- main_slider_container end -->
+
+          <div id="t7_more_filters">
+            <a class="trigger_2 inactive" href="#">more filters</a>
+          </div><!-- t7_more_filters end -->
+        </div><!-- 1ST_SET END -->
+        <!-- ============ Second Filter Set Start ============ -->
+
+        <div class="second_set">
+          <div id="t7_dates">
+            move-in from <span id="date-from"></span> to <span id="date-to"></span>
+
+            <div id="slider-dates"></div><!-- slider-dates end -->
+          </div><!-- t7_dates end -->
+
+          <div class="amenities" id="amenities">
+            <ul class="left_col">
+              <li><input class="jquery-safari-checkbox" type="checkbox" name="amenities"
+              id="unit_am_balcony"><label for="unit_am_balcony">balcony</label></li>
+
+              <li><input class="jquery-safari-checkbox" type="checkbox" name="amenities"
+              id="unit_am_dishwasher"><label for=
+              "unit_am_dishwasher">dishwasher</label></li>
+
+              <li><input class="jquery-safari-checkbox" type=
+              "checkbox" name="amenities" id="unit_am_furnished"><label for=
+              "unit_am_furnished">furnished</label></li>
+
+              <li><input class="jquery-safari-checkbox" type=
+              "checkbox" name="amenities" id="unit_am_laundry"><label for=
+              "unit_am_laundry">unit laundry</label></li>
+
+              <li><input class="jquery-safari-checkbox" type=
+              "checkbox" name="amenities" id="unit_am_disposal"><label for=
+              "unit_am_disposal">disposal</label></li>
+            </ul>
+
+            <ul class="right_col">
+              <li><input class="jquery-safari-checkbox" type=
+              "checkbox" name="amenities" id="unit_am_garage"><label for=
+              "unit_am_garage">garage</label></li>
+
+              <li><input class="jquery-safari-checkbox" type=
+              "checkbox" name="amenities" id="building_am_pool"><label for=
+              "building_am_pool">pool</label></li>
+
+              <li><input class="jquery-safari-checkbox" type=
+              "checkbox" name="amenities" id="building_am_cats"><label for=
+              "building_am_cats">cats</label></li>
+
+              <li><input class="jquery-safari-checkbox" type=
+              "checkbox" name="amenities" id="building_am_dogs_small"><label for=
+              "building_am_dogs_small">small dogs</label></li>
+
+              <li><input class="jquery-safari-checkbox" type=
+              "checkbox" name="amenities" id="building_am_dogs_large"><label for=
+              "building_am_dogs_large">large dogs</label></li>
+            </ul>
+          </div><!-- amenities end -->
+        </div><!-- 2ND_SET END -->
+      </div><!-- BODY_CONTENT END -->
+    </div><!-- t7_ui_CONTAINER END -->
+  </div>
+</body>
+</html>
