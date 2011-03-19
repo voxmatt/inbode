12196e074827d6d745ad96180ac0954b95e3ae86
Ivan Stegic <ivan.stegic@gmail.com>
2011-03-19 10:03:28 -0500

    removed configuration items in drupal

--- inbode.com/src/sites/default/settings.php
+++ inbode.com/src/sites/default/settings.php
@@ -31,32 +31,21 @@ if (file_exists("/usr/www/users/stegic/git")) {
 	// inbode variables
 	// system path, depends on environment
 	$conf['i_syspath'] = '/usr/www/users/stegic/git/inbode/inbode.com/src';	
-	
-	// Google Account username and password
-	$conf['i_Guser'] = 'inbodemaps@gmail.com';
-	$conf['i_Gpass'] = 'w1r3dmag';
-	
-	// Google Maps unique ID
-	$conf['i_mid'] = '00048d00ffda2e6448768';
-	
+
 	// URI to Image server
 	$conf['i_imgsrv'] = 'http://inbode.ten7i.net/';
 	
 	// Inbode Application Name
-	$conf['i_appname'] = 'Inbode Alpha';
+	$conf['i_appname'] = 'Inbode Beta';
 
 	// fusion tables specific
 	// google account username
 	$conf['i_GAuser'] = 'maps@inbode.com';
 	// google account password	
 	$conf['i_GApass'] = 'w1r3dmag';
-	// google token for client login
-	$conf['i_GAtoken'] = 'DQAAAJ8BAAD_iDEUDg77F8rmBLZxkTQrUenastqFcUT73A-DgTyPE2dBKActL7hEqcOJ4Bn4EOY0sJJkKarzn9fcAjKIIBsCSTh36_nJJfSxxAhQGbQuckbc9TS0N7W2ZuHbAQ-nAT3XM3W3HOMe8jPWxD7Lz1gH2RrIeFG5losmnZzecXDxpNQz9Jxw-KaldOmMSa0rnf8q6yOQdfhr8BN7DTNac81EwamEJWVWnfHIlV9IlKo7fC_ejXbAKybjID0kxRENY-S9WCNWpeOX08cmdMUkygEmM6_-EAyMWvyXCTG9x99BzZMHddmH9FM3GmQ-GHUjwCNg-vekYhEbeO_f3DlFasRfasQXJSWHAJ2UsxVsJ9BsciRGXThL2jgBsEhn_aAaMaNiV5ov4ly1KDDCV7-4JYBgfWvbID87lmvNYPD0OO14TPUWYXCE8ma_PYZMOgxhKFuDFo2k2W_NlbC5cSF26_ruRX-nizIlm7fulPrd3VQ-wtInQXrkunjEthcBLNqY324b3t_niRgEkgSzBM99siqfkhpfp40_Vkm-eofMhdhp_A';
+	// fusion table datasrc id
 	$conf['i_InbodeBeta-BuildingsUnits'] = '417990';
-	// to be used in the future
-	$conf['i_InbodeBeta-Building'] = '000000';
-	$conf['i_InbodeBeta-Unit'] = '000000';
-	
+
 	
 } else if (file_exists("/usr/www/users/inbode")) {
 
@@ -69,31 +58,19 @@ if (file_exists("/usr/www/users/stegic/git")) {
 	// system path, depends on environment
 	$conf['i_syspath'] = '/usr/www/users/inbode/git/inbode/inbode.com/src';
 	
-	// Google Account username and password
-	$conf['i_Guser'] = 'inbodemaps@gmail.com';
-	$conf['i_Gpass'] = 'w1r3dmag';
-	
-	// Google Maps unique ID
-	$conf['i_mid'] = '00048d00ffda2e6448768';
-	
 	// URI to Image server
 	$conf['i_imgsrv'] = 'http://inbode.com/';
 	
 	// Inbode Application Name
-	$conf['i_appname'] = 'Inbode Alpha';
+	$conf['i_appname'] = 'Inbode Beta';
 
 	// fusion tables specific
 	// google account username
 	$conf['i_GAuser'] = 'maps@inbode.com';
 	// google account password	
 	$conf['i_GApass'] = 'w1r3dmag';
-	// google token for client login
-	$conf['i_GAtoken'] = 'DQAAAJ8BAAD_iDEUDg77F8rmBLZxkTQrUenastqFcUT73A-DgTyPE2dBKActL7hEqcOJ4Bn4EOY0sJJkKarzn9fcAjKIIBsCSTh36_nJJfSxxAhQGbQuckbc9TS0N7W2ZuHbAQ-nAT3XM3W3HOMe8jPWxD7Lz1gH2RrIeFG5losmnZzecXDxpNQz9Jxw-KaldOmMSa0rnf8q6yOQdfhr8BN7DTNac81EwamEJWVWnfHIlV9IlKo7fC_ejXbAKybjID0kxRENY-S9WCNWpeOX08cmdMUkygEmM6_-EAyMWvyXCTG9x99BzZMHddmH9FM3GmQ-GHUjwCNg-vekYhEbeO_f3DlFasRfasQXJSWHAJ2UsxVsJ9BsciRGXThL2jgBsEhn_aAaMaNiV5ov4ly1KDDCV7-4JYBgfWvbID87lmvNYPD0OO14TPUWYXCE8ma_PYZMOgxhKFuDFo2k2W_NlbC5cSF26_ruRX-nizIlm7fulPrd3VQ-wtInQXrkunjEthcBLNqY324b3t_niRgEkgSzBM99siqfkhpfp40_Vkm-eofMhdhp_A';
+	// fusion table datasrc id
 	$conf['i_InbodeBeta-BuildingsUnits'] = '422613';
-	// to be used in the future
-	$conf['i_InbodeBeta-Building'] = '000000';
-	$conf['i_InbodeBeta-Unit'] = '000000';
-
 
 } else if (file_exists("/Users/ivan/git/inbode/inbode.com/src")) {
 
@@ -106,30 +83,19 @@ if (file_exists("/usr/www/users/stegic/git")) {
 	// system path, depends on environment
 	$conf['i_syspath'] = '/Users/ivan/git/inbode/inbode.com/src';		// Ivan
 	
-	// Google Account username and password
-	$conf['i_Guser'] = 'inbodemaps@gmail.com';
-	$conf['i_Gpass'] = 'w1r3dmag';
-	
-	// Google Maps unique ID
-	$conf['i_mid'] = '00048d00ffda2e6448768';
-	
 	// URI to Image server
 	$conf['i_imgsrv'] = 'http://inbode/';
 	
 	// Inbode Application Name
-	$conf['i_appname'] = 'Inbode Alpha';
+	$conf['i_appname'] = 'Inbode Beta';
 
 	// fusion tables specific
 	// google account username
 	$conf['i_GAuser'] = 'maps@inbode.com';
 	// google account password	
 	$conf['i_GApass'] = 'w1r3dmag';
-	// google token for client login
-	$conf['i_GAtoken'] = 'DQAAAJ8BAAD_iDEUDg77F8rmBLZxkTQrUenastqFcUT73A-DgTyPE2dBKActL7hEqcOJ4Bn4EOY0sJJkKarzn9fcAjKIIBsCSTh36_nJJfSxxAhQGbQuckbc9TS0N7W2ZuHbAQ-nAT3XM3W3HOMe8jPWxD7Lz1gH2RrIeFG5losmnZzecXDxpNQz9Jxw-KaldOmMSa0rnf8q6yOQdfhr8BN7DTNac81EwamEJWVWnfHIlV9IlKo7fC_ejXbAKybjID0kxRENY-S9WCNWpeOX08cmdMUkygEmM6_-EAyMWvyXCTG9x99BzZMHddmH9FM3GmQ-GHUjwCNg-vekYhEbeO_f3DlFasRfasQXJSWHAJ2UsxVsJ9BsciRGXThL2jgBsEhn_aAaMaNiV5ov4ly1KDDCV7-4JYBgfWvbID87lmvNYPD0OO14TPUWYXCE8ma_PYZMOgxhKFuDFo2k2W_NlbC5cSF26_ruRX-nizIlm7fulPrd3VQ-wtInQXrkunjEthcBLNqY324b3t_niRgEkgSzBM99siqfkhpfp40_Vkm-eofMhdhp_A';
+	// fusion table datasrc id
 	$conf['i_InbodeBeta-BuildingsUnits'] = '517005';
-	// to be used in the future
-	$conf['i_InbodeBeta-Building'] = '000000';
-	$conf['i_InbodeBeta-Unit'] = '000000';
 
 
 } else {
@@ -143,30 +109,19 @@ if (file_exists("/usr/www/users/stegic/git")) {
 	// system path, depends on environment
 	$conf['i_syspath'] = '/Users/william/git/inbode/inbode.com/src';		// William
 	
-	// Google Account username and password
-	$conf['i_Guser'] = 'inbodemaps@gmail.com';
-	$conf['i_Gpass'] = 'w1r3dmag';
-	
-	// Google Maps unique ID
-	$conf['i_mid'] = '00048d00ffda2e6448768';
-	
 	// URI to Image server
 	$conf['i_imgsrv'] = 'http://inbode/';
 	
 	// Inbode Application Name
-	$conf['i_appname'] = 'Inbode Alpha';
+	$conf['i_appname'] = 'Inbode Beta';
 
 	// fusion tables specific
 	// google account username
 	$conf['i_GAuser'] = 'maps@inbode.com';
 	// google account password	
 	$conf['i_GApass'] = 'w1r3dmag';
-	// google token for client login
-	$conf['i_GAtoken'] = 'DQAAAJ8BAAD_iDEUDg77F8rmBLZxkTQrUenastqFcUT73A-DgTyPE2dBKActL7hEqcOJ4Bn4EOY0sJJkKarzn9fcAjKIIBsCSTh36_nJJfSxxAhQGbQuckbc9TS0N7W2ZuHbAQ-nAT3XM3W3HOMe8jPWxD7Lz1gH2RrIeFG5losmnZzecXDxpNQz9Jxw-KaldOmMSa0rnf8q6yOQdfhr8BN7DTNac81EwamEJWVWnfHIlV9IlKo7fC_ejXbAKybjID0kxRENY-S9WCNWpeOX08cmdMUkygEmM6_-EAyMWvyXCTG9x99BzZMHddmH9FM3GmQ-GHUjwCNg-vekYhEbeO_f3DlFasRfasQXJSWHAJ2UsxVsJ9BsciRGXThL2jgBsEhn_aAaMaNiV5ov4ly1KDDCV7-4JYBgfWvbID87lmvNYPD0OO14TPUWYXCE8ma_PYZMOgxhKFuDFo2k2W_NlbC5cSF26_ruRX-nizIlm7fulPrd3VQ-wtInQXrkunjEthcBLNqY324b3t_niRgEkgSzBM99siqfkhpfp40_Vkm-eofMhdhp_A';
+	// fusion table datasrc id
 	$conf['i_InbodeBeta-BuildingsUnits'] = '517005';
-	// to be used in the future
-	$conf['i_InbodeBeta-Building'] = '000000';
-	$conf['i_InbodeBeta-Unit'] = '000000';
 
 
 }
