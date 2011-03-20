7bc3f52a5b6ddc2caa86aa7cdd5a4ec107d55559
Ivan Stegic <ivan.stegic@gmail.com>
2011-03-19 20:35:57 -0500

    fixed placement of dropdown for autocomplete on building pages; added background teal color on hover and also a hand not arrow on hover.

--- inbode.com/src/sites/all/themes/inbode/script.js
+++ inbode.com/src/sites/all/themes/inbode/script.js
@@ -2,3 +2,28 @@ $(document).ready(function() {
 
 	
 });
+
+
+Drupal.jsAC.prototype.populatePopup = function () {
+  // Show popup
+  if (this.popup) {
+    $(this.popup).remove();
+  }
+  this.selected = false;
+  this.popup = document.createElement('div');
+  this.popup.id = 'autocomplete';
+  this.popup.owner = this;
+  var inputPosition = $(this.input).position();
+  var inputWrapperPosition = $('div#'+$(this.input).attr('id')+'-wrapper').position();
+  $(this.popup).css({
+    marginTop: this.input.offsetHeight +'px',
+    marginLeft: (inputPosition.left-inputWrapperPosition.left)+'px',
+    width: (this.input.offsetWidth - 4) +'px',
+    display: 'none'
+  });
+  $(this.input).before(this.popup);
+
+  // Do search
+  this.db.owner = this;
+  this.db.search(this.input.value);
+};
