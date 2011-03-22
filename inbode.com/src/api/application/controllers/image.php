b05d770068e8809fad8a4b49e805c3a178389df8
inbode <inbode@inbode.com>
2011-03-22 12:29:32 -0400

    removed conditional image rendering

--- inbode.com/src/api/application/controllers/image.php
+++ inbode.com/src/api/application/controllers/image.php
@@ -7,18 +7,10 @@ class Image extends Controller {
 		// this function generates the inbode logo as a png,
 		// and it also records the unique unit number hit if it exists
 
+
 		if ( is_numeric($unit) ) {
 
-			if ( isset($_SERVER['HTTP_REFERER'])) {
 
-				$pos = strpos( strtolower($_SERVER['HTTP_REFERER']), 'craigslist.org');
-				
-				if($pos === false) {
-				
-				} else {
-					// only log if the refering domain name is craigslist.org
-					log_message('error', $_SERVER['HTTP_REFERER']);		
-	
 					$this->load->database();
 					$getquery = $this->db->get_where('content_type_unit', array('nid' => $unit));
 					$r = $getquery->result_array();
@@ -32,12 +24,6 @@ class Image extends Controller {
 						$this->db->where('nid', $unit);
 						$this->db->update('content_type_unit', $data); 
 					}	
-				}
-
-
-
-			}
-
 
 		}
 	
